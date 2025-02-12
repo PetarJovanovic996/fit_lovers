702daf0fcaae702e75528e4ba01e518e69a065ef
PetarJovanovic996 <jovanovicp996@gmail.com>
2025-02-12 21:17:53 +0100

    feat(auth&language): Added dark theme

--- lib/core/my_theme.dart
+++ lib/core/my_theme.dart
@@ -49,4 +49,50 @@ class MyTheme {
       ),
     );
   }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primaryColor: const Color.fromARGB(255, 30, 30, 30),
+      hintColor: const Color.fromARGB(255, 160, 160, 160),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      scaffoldBackgroundColor: const Color.fromARGB(255, 18, 18, 18),
+      appBarTheme: AppBarTheme(
+        color: const Color.fromARGB(255, 22, 41, 60),
+        elevation: 0,
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      textTheme: TextTheme(
+        bodyLarge: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.normal,
+          color: Colors.white70,
+        ),
+        titleLarge: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      cardTheme: CardTheme(
+        color: Colors.grey[850],
+        elevation: 4,
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      listTileTheme: ListTileThemeData(
+        contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        tileColor: Colors.grey[800],
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    );
+  }
 }
