e42dcb875ed677a3907af35afa9b97ae785f9d68
Petar Jovanovic <petarjovanovic@Mac.home>
2025-03-08 18:30:02 +0100

    Resolve filtering & search bar

--- lib/presentations/screens/home_screen_tabs/home_tab_content.dart
+++ lib/presentations/screens/home_screen_tabs/home_tab_content.dart
@@ -11,41 +11,50 @@ class HomeTabContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: Column(
         children: [
           _Filters(),
-          Expanded(
-            child: BlocBuilder<ExerciseCubit, ExerciseState>(
-              builder: (context, state) {
-                if (state is ExerciseLoading) {
-                  return LoadingWidget();
-                }
-
-                if (state is ExerciseLoaded) {
-                  return RefreshIndicator(
-                    color: Colors.black,
-                    onRefresh: () async {
-                      context.read<FiltersCubit>().clearFilters();
-                      await context.read<ExerciseCubit>().fetchAllExercises();
-                    },
-                    child: ListView.builder(
-                        itemCount: state.exercises.length,
-                        itemBuilder: (context, index) => ExerciseItem(
-                              exercise: state.exercises[index],
-                            )),
-                  );
-                }
-
-                if (state is ExerciseError) {
-                  return Center(child: Text(state.message));
-                }
-                return Container();
-              },
-            ),
-          ),
+          ExercisesContent(),
         ],
+      ),
+    );
+  }
+}
+
+class ExercisesContent extends StatelessWidget {
+  const ExercisesContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: BlocBuilder<ExerciseCubit, ExerciseState>(
+        builder: (context, state) {
+          if (state is ExerciseLoading) {
+            return const LoadingWidget();
+          }
+
+          if (state is ExerciseLoaded) {
+            return RefreshIndicator(
+              color: Colors.black,
+              onRefresh: () async {
+                context.read<FiltersCubit>().clearFilters();
+                await context.read<ExerciseCubit>().fetchAllExercises();
+              },
+              child: ListView.builder(
+                  itemCount: state.exercises.length,
+                  itemBuilder: (context, index) => ExerciseItem(
+                        exercise: state.exercises[index],
+                      )),
+            );
+          }
+
+          if (state is ExerciseError) {
+            return Center(child: Text(state.message));
+          }
+          return Container();
+        },
       ),
     );
   }
@@ -62,39 +71,23 @@ class _Filters extends StatelessWidget {
         builder: (context, state) {
           return Column(
             children: [
-              _TypeFilter(),
-              SizedBox(
+              const _TypeFilter(),
+              const SizedBox(
                 height: 24,
               ),
               Row(
                 children: [
-                  Expanded(
-                    child: TextFormField(
-                      initialValue: state.searchByName,
-
-                      // s pecom:
-                      // na reset ne micu se slova iz textformfield/a
-                      onChanged: (value) {
-                        context.read<FiltersCubit>().searchByName(value);
-                      },
-
-                      decoration: InputDecoration(
-                        labelText:
-                            AppLocalizations.of(context)!.searchExercises,
-                        prefixIcon: Icon(Icons.search),
-                      ),
-                    ),
-                  ),
+                  const SearchField(),
                   IconButton(
                       onPressed: () {
                         context
                             .read<ExerciseCubit>()
                             .fetchFilteredExercises(name: state.searchByName);
                       },
-                      icon: Icon(Icons.filter_list_rounded)),
+                      icon: const Icon(Icons.filter_list_rounded)),
                 ],
               ),
-              SizedBox(
+              const SizedBox(
                 height: 24,
               ),
             ],
@@ -105,49 +98,102 @@ class _Filters extends StatelessWidget {
   }
 }
 
+class SearchField extends StatefulWidget {
+  const SearchField({super.key});
+
+  @override
+  State<SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<FiltersCubit, FiltersState>(
+      listenWhen: (prev, curr) => prev.searchByName != curr.searchByName,
+      listener: (context, state) {
+        if (state.searchByName == null) {
+          _controller.clear();
+          FocusScope.of(context).unfocus();
+        }
+      },
+      child: Expanded(
+        child: TextFormField(
+          controller: _controller,
+          onChanged: (value) =>
+              context.read<FiltersCubit>().searchByName(value),
+          decoration: InputDecoration(
+            labelText: AppLocalizations.of(context)!.searchExercises,
+            prefixIcon: const Icon(Icons.search),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _TypeFilter extends StatelessWidget {
   const _TypeFilter();
 
   @override
   Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: ListView.separated(
+        separatorBuilder: (context, index) => const SizedBox(width: 8),
+        scrollDirection: Axis.horizontal,
+        itemCount: ExerciseType.values.length,
+        itemBuilder: (context, index) => CustomFilterButton(
+          type: ExerciseType.values[index],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomFilterButton extends StatelessWidget {
+  const CustomFilterButton({required this.type, super.key});
+
+  final ExerciseType type;
+
+  @override
+  Widget build(BuildContext context) {
     return BlocBuilder<FiltersCubit, FiltersState>(
+      buildWhen: (prev, curr) => prev.type != curr.type,
       builder: (context, state) {
-        return SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            spacing: 8,
-            children: [
-              _buildFilterButton(context, ExerciseType.cardio, 'Cardio'),
-              _buildFilterButton(context, ExerciseType.olympic_weightlifting,
-                  'Olympic Weightlifting'),
-              _buildFilterButton(
-                  context, ExerciseType.plyometrics, 'Plyometrics'),
-              _buildFilterButton(
-                  context, ExerciseType.powerlifting, 'Powerlifting'),
-              _buildFilterButton(context, ExerciseType.strength, 'Strength'),
-              _buildFilterButton(
-                  context, ExerciseType.stretching, 'Stretching'),
-              _buildFilterButton(context, ExerciseType.strongman, 'Strongman'),
-            ],
+        return ElevatedButton(
+          style: ButtonStyle(
+            side: WidgetStatePropertyAll(
+              BorderSide(
+                color: state.type == type ? Colors.blue : Colors.white,
+              ),
+            ),
+          ),
+          onPressed: () {
+            context.read<FiltersCubit>().selectType(type);
+            context.read<ExerciseCubit>().fetchFilteredExercises(
+                  type: type.name,
+                );
+          },
+          child: Text(
+            type.getScreenName,
+            style: const TextStyle(color: Colors.black),
           ),
         );
       },
-    );
-  }
-
-  ElevatedButton _buildFilterButton(
-      BuildContext context, ExerciseType type, String title) {
-    return ElevatedButton(
-      onPressed: () {
-        context.read<FiltersCubit>().selectType(type);
-        context.read<ExerciseCubit>().fetchFilteredExercises(
-              type: type.toString().split('.').last,
-            );
-      },
-      child: Text(
-        title,
-        style: TextStyle(color: Colors.black),
-      ),
     );
   }
 }
