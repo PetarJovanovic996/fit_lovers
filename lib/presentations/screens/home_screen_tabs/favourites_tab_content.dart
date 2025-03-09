dab537252ed9d29a6d26cd778be3bc3d51103979
PetarJovanovic996 <jovanovicp996@gmail.com>
2025-03-09 14:40:36 +0100

    feat(fav): Lose

--- lib/presentations/screens/home_screen_tabs/favourites_tab_content.dart
+++ lib/presentations/screens/home_screen_tabs/favourites_tab_content.dart
@@ -1,10 +1,43 @@
+import 'package:fit_lovers/presentations/cubit/exercises/exercise_cubit.dart';
+import 'package:fit_lovers/presentations/widgets/exercise_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fit_lovers/presentations/cubit/favourites/favourites_cubit.dart';
+import 'package:fit_lovers/presentations/widgets/loading_widget.dart';
 
 class FavouritesTabContent extends StatelessWidget {
   const FavouritesTabContent({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('FavouritesTab'));
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: BlocBuilder<FavouritesCubit, FavouritesState>(
+        builder: (context, state) {
+          if (state is FavouritesLoading) {
+            return const LoadingWidget();
+          }
+
+          if (state is FavouritesLoaded) {
+            return ListView.builder(
+              itemCount: state.favourites.length,
+              itemBuilder: (context, index) {
+                final exercise = state.favourites[index];
+
+                return ExerciseItem(
+                  exercise: exercise,
+                );
+              },
+            );
+          }
+
+          if (state is FavouritesError) {
+            return Center(child: Text(state.message));
+          }
+
+          return Container();
+        },
+      ),
+    );
   }
 }
